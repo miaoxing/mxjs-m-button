e979e37e3a201297f6d750e7eba1b8c6875b4fea
twinh <twinhuang@qq.com>
2021-06-07 17:40:33 +0800

    feat(m-button): 增加 `Button` 和 `ButtonGroup` 组件

---
+++ index.js
@@ -0,0 +1,33 @@
+import {Button as BaseButton, View} from '@fower/taro';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import './index.scss';
+
+const Button = ({variant, className, children, ...props}) => {
+  return (
+    <BaseButton className={clsx('mx-btn', variant && ('mx-btn-' + variant), className)} {...props}>
+      {children}
+    </BaseButton>
+  );
+};
+
+const ButtonGroup = ({className, children, ...props}) => {
+  return (
+    <View className={clsx('mx-btn-group', className)} {...props}>{children}</View>
+  );
+};
+
+Button.propTypes = {
+  variant: PropTypes.string,
+  size: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
+
+ButtonGroup.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default Button;
+export {Button, ButtonGroup};
