17c2d448beab76e25a92f6f55078449dcf434bbf
twinh <twinhuang@qq.com>
2021-07-02 18:35:22 +0800

    test(m-button): 增加 `Button` 和 `ButtonGroup` 组件

---
+++ __tests__/Button.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from '..';
+import {render} from '@testing-library/react';
+
+describe('Button', () => {
+  test('basic', () => {
+    const {container} = render(<Button>test</Button>);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('variant: primary', () => {
+    const {container} = render(<Button variant="primary">primary</Button>);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('variant: secondary', () => {
+    const {container} = render(<Button variant="secondary">secondary</Button>);
+    expect(container).toMatchSnapshot();
+  });
+});
